ad0835c3095a1cc558676cf3b37d9b792e591c43
Dias29 <33831288+Dias29@users.noreply.github.com>
2017-11-22 23:53:53 +0600

    prackt task

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,16 +11,33 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             //Задание 1
-            Console.WriteLine("Дано выражение y=7x^2-3x+4");
-            Console.Write("Введите значение х: ");
-            int x = int.Parse(Console.ReadLine());
-            var y = (7 * Math.Pow(4, 2)) - (3 * x) + 4;
-            Console.WriteLine("Ответ:\n " + "7*" + x + "^2" + "-3*" + x + "+4 = " + y);
+            //Console.WriteLine("Дано выражение y=7x^2-3x+4");
+            //Console.Write("Введите значение х: ");
+            //int x = int.Parse(Console.ReadLine());
+            //var y = (7 * Math.Pow(4, 2)) - (3 * x) + 4;
+            //Console.WriteLine("Ответ:\n " + "7*" + x + "^2" + "-3*" + x + "+4 = " + y);
 
 
             //Задание 2
-            Console.Write("Введите радиус окружности: ");
+            //Console.Write("Введите радиус окружности(cm): ");
+            //int r = int.Parse(Console.ReadLine());
+            //const double pi = 3.14;
+            //Console.WriteLine("Длина окружности - " + 2*pi*r+" cm");
+            //Console.Write("Площадь круга - " + pi * Math.Pow(r, 2)+" cm2");
 
+            //Задание 3
+            //Console.Write("Введите расстояние в см: ");
+            //int distance = int.Parse(Console.ReadLine());
+            //int cmInMeter = 100;
+            //int meter = distance / cmInMeter;
+            //Console.Write("Сдесь " + meter + " метров");
+
+            //Задание 4
+            //Console.WriteLine("C 01.04.2017 прошло 234 дня");
+            //int days = 7;
+            //int afterMoment = 234;
+            //int week = afterMoment / days;
+            //Console.Write("Всего недель прошло - " + week);
 
 
             Console.ReadLine();
